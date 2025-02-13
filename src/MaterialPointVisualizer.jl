8bb9d89f62fdfe446f97ddeb8f27e653563e558a
ZenanH <zenan.huo@outlook.com>
2025-02-13 13:28:48 +0000

    🧘🏼‍♂ Initialize `trimesh` and enhance environment check

--- src/MaterialPointVisualizer.jl
+++ src/MaterialPointVisualizer.jl
@@ -11,17 +11,20 @@
 
 module MaterialPointVisualizer
 
-using CondaPkg, DelimitedFiles, HDF5, NearestNeighbors, PlyIO, Printf, ProgressMeter, 
-      PythonCall, WriteVTK
+using CondaPkg, DelimitedFiles, HDF5, NearestNeighbors, Printf, ProgressMeter, PythonCall, 
+      WriteVTK
       
 import MaterialPointSolver: DeviceArgs2D, DeviceGrid2D, DeviceParticle2D, DeviceProperty,
                             DeviceArgs3D, DeviceGrid3D, DeviceParticle3D     
+
+const trimesh = Ref{Py}()
 
 include("mpm2vtp.jl")
 include("particle2vtp.jl")
 include("particle2surf.jl")
 
 function __init__()
+    @info "checking environment..."
     try
         run(pipeline(`splashsurf -V`, stdout=devnull, stderr=devnull))
     catch e
@@ -33,6 +36,7 @@ function __init__()
             """
         end
     end
+    trimesh[] = pyimport("trimesh")
 end
 
 end
